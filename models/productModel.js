957869d30e69819d9805cdeea1f6c9b744e4b2a2
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-20 10:14:20 +0530

    update product model

---
+++ models/productModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productModel = mongoose.model("Product", productSchema);
+
+module.exports = productModel;
