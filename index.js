7aa45b8f0e6e5879d939b0e900ed59574d7bbee4
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-20 21:43:44 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 require("./config/database.js");
 const userRoutes = require("./Routes/userRoutes");
@@ -16,6 +17,7 @@ if (!process.env.PORT) {
   );
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
